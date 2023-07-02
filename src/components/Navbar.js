8dd9a28003daa1126b96e65d17303a2e5639eb7c
Viktor <vneshikj@gmail.com>
2023-07-02 16:51:41 +1200

    Progress made with the navigation bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,12 @@ import "../App.css";
 function Navbar() {
   return (
     <div className="navbar">
-      <p>Hello</p>
+      <a className="signature">V.Neshikj</a>
+      <a className="seperator">|</a>
+      <a>About me</a>
+      <a>Projects</a>
+      <a>Contact</a>
+      <a>Resume</a>
     </div>
   );
 }
