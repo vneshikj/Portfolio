bcf22ba120257a20d58031b4c7562cf56ff4c916
Viktor <vneshikj@gmail.com>
2023-07-02 23:16:54 +1200

    Started on project section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import AboutMe from "./components/AboutMe";
+import Projects from "./components/Projects";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <AboutMe />
+      <Projects />
     </div>
   );
 }
---
+++ src/components/Projects.js
@@ -0,0 +1,19 @@
+import "../App.css";
+
+function Projects() {
+  return (
+    <div className="projects-section">
+      <a className="name">
+        <h1 className="purple-headers-generic">Projects</h1>
+      </a>
+      <div className="projects=layout">
+        <div className="project-1"></div>
+        <div className="project-2"></div>
+        <div className="project-3"></div>
+        <div className="project-4"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Projects;
