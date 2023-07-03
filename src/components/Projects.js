2fd00bbc9830dc880c24dd77b811c3254cc5803d
Viktor <vneshikj@gmail.com>
2023-07-03 14:21:28 +1200

    Project section finished with refactoring needed for scalability

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,11 +6,57 @@ function Projects() {
       <a className="name">
         <h1 className="purple-headers-generic">Projects</h1>
       </a>
-      <div className="projects=layout">
-        <div className="project-1"></div>
-        <div className="project-2"></div>
-        <div className="project-3"></div>
-        <div className="project-4"></div>
+      <div className="projects-layout">
+        <div className="project">
+          <p className="project-title">Flappy Bird</p>
+
+          <hr className="rounded"></hr>
+
+          <p className="project-content">
+            This flappy bird clone was developed using VHDL and deployed on a
+            Cyclone 5 FPGA. This project involved the design of a finite state
+            machine, as well as components for VGA output and synchronization.
+            The game features different levels, power-ups, and custom character
+            selection.
+          </p>
+        </div>
+        <div className="project">
+          <p className="project-title">Interpreter</p>
+
+          <hr className="rounded"></hr>
+
+          <p className="project-content">
+            An ASL sign language interpreter built in Python using PyTorch and
+            PyQt5 for the UI. An MVC design pattern was used to streamline the
+            development. The interpreter features three different AI models
+            which can translate ASL. Users can also upload their own photos or
+            use the camera feed within the app.
+          </p>
+        </div>
+        <div className="project">
+          <p className="project-title">WatchList</p>
+
+          <hr className="rounded"></hr>
+
+          <p className="project-content">
+            WatchList is an Android application used for showcasing watches. The
+            app was built using Java and Android Studio and features search
+            functionality, filtering by categories, and a favorites page.
+          </p>
+        </div>
+        <div className="project">
+          <p className="project-title">Energy Monitor</p>
+
+          <hr className="rounded"></hr>
+
+          <p className="project-content">
+            An embedded system project that monitors the real-time energy usage
+            of an inductive appliance. This project features a custom circuit
+            board for signal sensing and conditioning. The circuit board was
+            paired with an ATmega328PB microprocessor running firmware for
+            signal processing.
+          </p>
+        </div>
       </div>
     </div>
   );
