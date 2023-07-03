ecf1a20018fafe0cf0642a75b71614fbdb7c849d
Viktor <vneshikj@gmail.com>
2023-07-03 17:42:03 +1200

    Started working on shields

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,22 +5,27 @@ function Skills() {
     <div className="technical-skills-section">
       <h1 className="purple-headers-generic2">Technical Skills</h1>
 
-      <div className="skil;-container">
-        <h2 className="purple-mini-headeder">
-          Prgramming Languages and Markup
-        </h2>
+      <div className="skill-container">
+        <h2 className="purple-mini-header">Prgramming Languages and Markup</h2>
+
+        <div className="shield-container">
+          <img
+            alt="CSS3"
+            src="https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white"
+          />
+        </div>
       </div>
 
-      <div className="skil;-container">
-        <h2 className="purple-mini-headeder">Embedded Systems</h2>
+      <div className="skill-container">
+        <h2 className="purple-mini-header">Embedded Systems</h2>
       </div>
 
-      <div className="skil;-container">
-        <h2 className="purple-mini-headeder">Frameworks and Libraries</h2>
+      <div className="skill-container">
+        <h2 className="purple-mini-header">Frameworks and Libraries</h2>
       </div>
 
-      <div className="skil;-container">
-        <h2 className="purple-mini-headeder">Other Tools</h2>
+      <div className="skill-container">
+        <h2 className="purple-mini-header">Other Tools</h2>
       </div>
 
       <div></div>
