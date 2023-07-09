a48f6b8129667b4f83d87bc8406adeaa2478df64
Viktor <vneshikj@gmail.com>
2023-07-09 15:19:10 +1200

    Imported dependencies, decent progress made with contact form, working on styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,12 @@
 import "../App.css";
+import ContactForm from "./ContactForm";
 
 function Contact() {
   return (
     <div className="contact-section">
-      <div className="contact-form"></div>
+      <div className="contact-form">
+        <ContactForm />
+      </div>
       <div className="contact-writing">
         <h2>Contact Me</h2>
       </div>
---
+++ src/components/ContactForm.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+const ContactForm = () => {
+  const [status, setStatus] = useState("Submit");
+  const handleSubmit = async (e) => {
+    // asynch function doesnt block execution of other code, await statement pauses execution of function until promise is resolved
+    e.preventDefault();
+    setStatus("Sending...");
+    const { name, email, message } = e.target.elements; // e.target.elements allows access to values by id or name attributes, target is whole form
+    let details = {
+      name: name.value,
+      email: email.value,
+      message: message.value,
+    };
+    let response = await fetch("http://localhost:3000/contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(details),
+    });
+    setStatus("Submit");
+    let result = await response.json();
+    alert(result.status); // Alerts if message is sent correctly
+  };
+  return (
+    <form className="submission-form" onSubmit={handleSubmit}>
+      <div>
+        <input type="text" id="name" placeholder="Name:" required />
+      </div>
+      <div>
+        <input
+          type="email"
+          id="email"
+          placeholder="Valid email address:"
+          required
+        />
+      </div>
+      <div>
+        <textarea id="message" required placeholder="Message:" />
+      </div>
+      <button type="submit">{status}</button>
+    </form>
+  );
+};
+
+export default ContactForm;
