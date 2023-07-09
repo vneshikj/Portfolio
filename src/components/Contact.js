e655dd2178cc72963b720e53fd3ba0d704e16f94
Viktor <vneshikj@gmail.com>
2023-07-09 21:01:55 +1200

    Styling nearly finished for whole project

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,6 +7,10 @@ function Contact() {
       <ContactForm />
       <div className="contact-writing">
         <h1>Contact Me</h1>
+        <p className="contact-description">
+          I welcome the opportunity for feedback, collaborative projects or even
+          just a casual conversation!
+        </p>
       </div>
     </div>
   );
