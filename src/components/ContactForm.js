8998e7ebb0bbe3d3dd100e091ac2a08f21a57e7e
Viktor <vneshikj@gmail.com>
2023-07-11 19:05:12 +1200

    Inital development of contact form using Netlify

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -24,13 +24,20 @@ const ContactForm = () => {
     alert(result.status); // Alerts if message is sent correctly
   };
   return (
-    <form className="submission-form" onSubmit={handleSubmit}>
+    <form
+      name="contact v1"
+      method="post"
+      data-netlify="true"
+      className="submission-form"
+      onSubmit="submit"
+    >
       <div className="joiner">
         <div>
+          <input type="hidden" name="form-name" value="contact v1" />
           <input
             className="name-input"
+            name="name"
             type="text"
-            id="name"
             placeholder="Name:"
             required
           />
@@ -38,15 +45,15 @@ const ContactForm = () => {
         <div>
           <input
             className="email-input"
+            name="email"
             type="email"
-            id="email"
             placeholder="Valid email address:"
             required
           />
         </div>
       </div>
       <div>
-        <textarea id="message" required placeholder="Message:" />
+        <textarea required placeholder="Message:" name="message" />
       </div>
       <button className="submit-button" type="submit">
         {status}
