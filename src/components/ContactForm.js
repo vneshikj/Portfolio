2d14b0b2b84d0f9d404a9b5e471d3fb52f39ec13
Viktor <vneshikj@gmail.com>
2023-07-11 19:12:54 +1200

    Reverting back and cleaning up

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,33 +1,11 @@
 import React, { useState } from "react";
 
 const ContactForm = () => {
-  const [status, setStatus] = useState("Submit");
-  const handleSubmit = async (e) => {
-    // asynch function doesnt block execution of other code, await statement pauses execution of function until promise is resolved
-    e.preventDefault();
-    setStatus("Sending...");
-    const { name, email, message } = e.target.elements; // e.target.elements allows access to values by id or name attributes, target is the whole form
-    let details = {
-      name: name.value,
-      email: email.value,
-      message: message.value,
-    };
-    let response = await fetch("http://localhost:3000/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify(details),
-    });
-    setStatus("Submit");
-    let result = await response.json();
-    alert(result.status); // Alerts if message is sent correctly
-  };
   return (
     <form
       name="contact v1"
       method="post"
-      netlify
+      data-netlify="true"
       className="submission-form"
       onSubmit="submit"
     >
@@ -56,7 +34,7 @@ const ContactForm = () => {
         <textarea required placeholder="Message:" name="message" />
       </div>
       <button className="submit-button" type="submit">
-        {status}
+        Submit
       </button>
     </form>
   );
